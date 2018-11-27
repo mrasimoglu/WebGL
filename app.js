582b960d3cdaa6cbf72514987211df213b131d45
Mustafa RASIMOGLU <mrasimoglu@hotmail.com>
2018-11-27 03:38:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ var RunDemo = function (vertexShaderText, fragmentShaderText, Images, Models) {
 	);
 	gl.bindTexture(gl.TEXTURE_2D, null);
 
+	//
 	var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
 	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
 	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
@@ -117,6 +118,7 @@ var RunDemo = function (vertexShaderText, fragmentShaderText, Images, Models) {
 	var worldMatrix = new Float32Array(16);
 	var viewMatrix = new Float32Array(16);
 	var projMatrix = new Float32Array(16);
+
 	mat4.identity(worldMatrix);
 	mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
 	mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
@@ -124,7 +126,7 @@ var RunDemo = function (vertexShaderText, fragmentShaderText, Images, Models) {
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-	
+
 	// Main render loop
 	var loop = function () {
 		gl.clearColor(0.75, 0.85, 0.8, 1.0);
